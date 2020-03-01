dc654f080836247c0fbf026550f5893323f2e5f5
Artem Gavrishchishen <artem.gavrishchishen@gmail.com>
2020-03-01 23:26:38 +0200

    normalize.css was added

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
-import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import './normalize.css';
+import './index.css';
 
 render(
   <BrowserRouter>
