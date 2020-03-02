435cc99395e3240e43a5032b6374fe990812231b
Artem Gavrishchishen <artem.gavrishchishen@gmail.com>
2020-03-03 00:23:19 +0200

    added lodash.throttle for handleResize

--- client/src/Desktop/Desktop.js
+++ client/src/Desktop/Desktop.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Desktop = () => {
+  console.log('Desktop');
   return (
     <div>
       <h1 style={{ textAlign: 'center', padding: '25px' }}>
--- client/src/Layout/components/WindowDimensionsProvider/WindowDimensionsProvider.js
+++ client/src/Layout/components/WindowDimensionsProvider/WindowDimensionsProvider.js
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
+import throttle from 'lodash.throttle';
 
 export const WindowDimensionsCtx = createContext(null);
 
@@ -12,17 +13,21 @@ const windowDims = () =>
 
 const WindowDimensionsProvider = ({ children }) => {
   const [dimensions, setDims] = useState(windowDims());
+
   const handleResize = () => {
     setDims(windowDims());
   };
+
   useEffect(() => {
+    const throttled = throttle(handleResize, 1000);
     if (typeof window !== 'undefined') {
-      window.addEventListener('resize', handleResize);
+      window.addEventListener('resize', throttled);
       return () => {
-        window.removeEventListener('resize', handleResize);
+        window.removeEventListener('resize', throttled);
       };
     }
   }, []);
+
   return (
     <WindowDimensionsCtx.Provider value={dimensions}>
       {children}
